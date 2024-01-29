b9b6344568544e45ae9d74c331749b6ee6dac719
IoT-Arduino <bunkajin@icloud.com>
2024-01-29 21:33:20 +0900

    Dialog ベース作成

--- src/Components/ColumnContainer.tsx
+++ src/Components/ColumnContainer.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { SortableContext, useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import { CiCirclePlus } from 'react-icons/ci'
@@ -7,6 +7,7 @@ import { BsSendArrowUp } from 'react-icons/bs'
 import { Column, Id, Drill } from './../types/types'
 import styles from './ColumnContainer.module.scss'
 import { DrillCard } from './DrillCard'
+import { Dialog } from './Dialog/Dialog'
 
 type Props = {
   column: Column
@@ -20,6 +21,7 @@ type Props = {
 
 export const ColumnContainer = (props: Props) => {
   const { column, drills, createDrill, deleteDrill, updateDrill, updateDrillStatus, submitDrill } = props
+  const [openDialog, setOpenDialog] = useState<boolean>(false)
 
   const drillsIds = useMemo(() => {
     return drills.map((drill) => drill.id)
@@ -44,49 +46,76 @@ export const ColumnContainer = (props: Props) => {
   }
 
   return (
-    <div ref={setNodeRef} style={style} className={styles['column-container']}>
-      {/* column title */}
-      <div {...attributes} {...listeners} className={styles['column-title']}>
-        <div>{column.title}</div>
+    <>
+      <div ref={setNodeRef} style={style} className={styles['column-container']}>
+        {/* column title */}
+        <div {...attributes} {...listeners} className={styles['column-title']}>
+          <div>{column.title}</div>
+        </div>
+        {/* column drill container */}
+        <div className={styles['column-drill-container']}>
+          <SortableContext items={drillsIds}>
+            {drills.map((drill) => (
+              <DrillCard
+                key={drill.id}
+                drill={drill}
+                columnId={column.id}
+                deleteDrill={deleteDrill}
+                updateDrill={updateDrill}
+                updateDrillStatus={updateDrillStatus}
+              />
+            ))}
+          </SortableContext>
+        </div>
+        {/* Column footer */}
+        {column.id === 'stock' && (
+          <button
+            className={styles['column-footer']}
+            onClick={() => {
+              createDrill(column.id)
+            }}
+          >
+            <CiCirclePlus />
+            ドリルを追加
+          </button>
+        )}
+        {column.id === 'drill' && (
+          <button
+            className={styles['column-footer']}
+            onClick={() => {
+              setOpenDialog(true)
+            }}
+          >
+            <BsSendArrowUp />
+            <span className={styles['column-submit-text']}>送信</span>
+          </button>
+        )}
       </div>
-      {/* column drill container */}
-      <div className={styles['column-drill-container']}>
-        <SortableContext items={drillsIds}>
-          {drills.map((drill) => (
-            <DrillCard
-              key={drill.id}
-              drill={drill}
-              columnId={column.id}
-              deleteDrill={deleteDrill}
-              updateDrill={updateDrill}
-              updateDrillStatus={updateDrillStatus}
-            />
-          ))}
-        </SortableContext>
-      </div>
-      {/* Column footer */}
-      {column.id === 'stock' && (
-        <button
-          className={styles['column-footer']}
-          onClick={() => {
-            createDrill(column.id)
-          }}
-        >
-          <CiCirclePlus />
-          ドリルを追加
-        </button>
-      )}
-      {column.id === 'drill' && (
-        <button
-          className={styles['column-footer']}
-          onClick={() => {
-            submitDrill()
-          }}
-        >
-          <BsSendArrowUp />
-          <span className={styles['column-submit-text']}>送信</span>
-        </button>
-      )}
-    </div>
+      <Dialog isOpen={openDialog} onClose={() => setOpenDialog(false)}>
+        <header>
+          <h2>タイトル</h2>
+        </header>
+        <section>コンテンツ</section>
+        <footer>
+          <button
+            type="button"
+            onClick={() => {
+              setOpenDialog(false)
+              submitDrill()
+            }}
+          >
+            送信
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setOpenDialog(false)
+            }}
+          >
+            キャンセル
+          </button>
+        </footer>
+      </Dialog>
+    </>
   )
 }
---
+++ src/Components/Dialog/Dialog.tsx
@@ -0,0 +1,65 @@
+import { useCallback, useEffect, useRef } from "react";
+import { RemoveScroll } from "react-remove-scroll";
+
+import classes from "./Dialog.module.scss";
+
+type Props = {
+  isOpen?: boolean;
+  children?: React.ReactNode;
+  onClose?: VoidFunction;
+};
+
+export const Dialog: React.FC<Props> = ({
+  isOpen = false,
+  children,
+  onClose,
+}:Props): React.ReactElement | null => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect((): void => {
+    const dialogElement = dialogRef.current;
+    if (!dialogElement) {
+      return;
+    }
+    if (isOpen) {
+      if (dialogElement.hasAttribute("open")) {
+        return;
+      }
+      dialogElement.showModal();
+    } else {
+      if (!dialogElement.hasAttribute("open")) {
+        return;
+      }
+      dialogElement.close();
+    }
+  }, [isOpen]);
+
+  const handleClickDialog = useCallback(
+    (event: React.MouseEvent<HTMLDialogElement>): void => {
+      [event];
+      onClose?.();
+    },
+    [onClose]
+  );
+
+  const handleClickContent = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>): void => {
+      event.stopPropagation();
+    },
+    []
+  );
+
+  return (
+    <RemoveScroll removeScrollBar enabled={isOpen}>
+      <dialog
+        className={classes["dialog"]}
+        ref={dialogRef}
+        onClick={handleClickDialog}
+      >
+        <div className={classes["content"]} onClick={handleClickContent}>
+          {children}
+        </div>
+      </dialog>
+    </RemoveScroll>
+  );
+};

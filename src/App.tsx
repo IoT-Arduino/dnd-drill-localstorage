0696b68cb5c5e75a655656a6124ee8046ad8ca7d
IoT-Arduino <bunkajin@icloud.com>
2024-01-31 13:34:45 +0900

    setup iconic react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import '@ionic/react/css/core.css';
+import { setupIonicReact } from '@ionic/react';
+
 import './App.css'
 import { MainBoard } from './Components/MainBoard'
+
+setupIonicReact();
 
 const App = () => {
   return (

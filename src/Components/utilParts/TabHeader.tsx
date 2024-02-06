886be96dedbde5a0df69cef9da1c5dc4443c18d7
IoT-Arduino <bunkajin@icloud.com>
2024-02-06 12:28:48 +0900

    TabHeader作成

--- src/Components/utilParts/TabHeader.tsx
+++ src/Components/utilParts/TabHeader.tsx
@@ -1,22 +1,46 @@
 import { useNavigate } from 'react-router-dom'
-import { IonHeader, IonLabel, IonSegment, IonSegmentButton, IonToolbar } from '@ionic/react'
+
+import styles from './TabHeader.module.scss'
+import { useEffect, useState } from 'react'
+
+import { useLocation } from 'react-router-dom';
+
+type ActiveTab = 'main' | 'history'
 
 export const TabHeader = () => {
   const navigate = useNavigate()
+  let location = useLocation();
+
+  const [activeTab, setActiveTab] = useState<ActiveTab>('main')
+
+  useEffect(()=>{
+    const currentTab = location.pathname.replace(/^\//, '') === 'history' ? 'history' : 'main'
+    setActiveTab(currentTab)
+  },[])
+
+
   return (
-    <>
-      <IonHeader>
-        <IonToolbar color="success" id="titleBar">
-          <IonSegment value="all">
-            <IonSegmentButton value="all" onClick={() => navigate('/')}>
-              <IonLabel>Mainboard</IonLabel>
-            </IonSegmentButton>
-            <IonSegmentButton value="history" onClick={() => navigate('/history')}>
-              <IonLabel>履歴</IonLabel>
-            </IonSegmentButton>
-          </IonSegment>
-        </IonToolbar>
-      </IonHeader>
-    </>
+    <div className={styles['tab-header-wrapper']} id="titleBar">
+      <div className={styles['tab-header-inner']}>
+        <p
+          className={activeTab === 'main' ? styles['tab-header-text-active'] : styles['tab-header-text']}
+          onClick={() => {
+            setActiveTab('main')
+            navigate('/')
+          }}
+        >
+          メイン
+        </p>
+        <p
+          className={activeTab === 'history' ? styles['tab-header-text-active'] : styles['tab-header-text']}
+          onClick={() => {
+            setActiveTab('history')
+            navigate('/history')
+          }}
+        >
+          履歴
+        </p>
+      </div>
+    </div>
   )
 }

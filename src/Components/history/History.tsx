ed7e35e7c63b4f3a4f84d4f0d58c4644bc6d9eb0
IoT-Arduino <bunkajin@icloud.com>
2024-02-06 14:39:33 +0900

    HIstoryPage css修正

--- src/Components/history/History.tsx
+++ src/Components/history/History.tsx
@@ -9,18 +9,24 @@ export const History = () => {
   return (
     <>
       <TabHeader />
-
-      <div  className={styles['history-wrapper']}>
-        {drillHistory.map((drill,i) => {
+      <div className={styles['history-wrapper']}>
+        {drillHistory.map((drill, i) => {
           return (
-            <div key={i}>
-              <p>{drill.date}</p>
-              <p>memo:{drill.memo}</p>
-              <ul>
-                {drill.drillItemsChecked.map((item) => {
-                  return <li key={item.id}>{item.content}</li>
-                })}
-              </ul>
+            <div>
+              <p className={styles['history-date']}>{drill.date}</p>
+              <div key={i} className={styles['history-item']}>
+                <p className={styles['history-memo']}>今日のメモ : {drill.memo}</p>
+                <p className={styles['history-drill-title']}>今日実施したドリル</p>
+                <ul className={styles['history-list']}>
+                  {drill.drillItemsChecked.map((item) => {
+                    return (
+                      <li key={item.id} className={styles['history-list-item']}>
+                        {item.content}
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
             </div>
           )
         })}

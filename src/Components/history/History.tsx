8e1aff3b51e36d7140e6dd5ec186becdcd8ad832
IoT-Arduino <bunkajin@icloud.com>
2024-02-06 12:23:26 +0900

    履歴ページcss（WIP）

--- src/Components/history/History.tsx
+++ src/Components/history/History.tsx
@@ -1,6 +1,8 @@
 import { useStorage } from '../../hooks/useStorage'
 import { TabHeader } from '../utilParts/TabHeader'
 
+import styles from './History.module.scss'
+
 export const History = () => {
   const { drillHistory } = useStorage()
 
@@ -8,7 +10,7 @@ export const History = () => {
     <>
       <TabHeader />
 
-      <div>
+      <div  className={styles['history-wrapper']}>
         {drillHistory.map((drill,i) => {
           return (
             <div key={i}>
